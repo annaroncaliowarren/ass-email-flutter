a38155eaa4d7a41770ba8ee726b1747e2b80fa59
Anna Cecília Roncalio <Anna.Roncalio@proway.treina>
2022-07-11 16:28:40 -0300

    [feat] Upload all files.

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Gerador de Assinatura - Email',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
